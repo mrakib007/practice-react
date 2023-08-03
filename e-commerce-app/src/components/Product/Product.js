67c9ce25b3b39d1ee60530e00c2cab5e249368f5
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-08-03 18:49:40 +0600

    Added button and fixed the position

--- e-commerce-app/src/components/Product/Product.js
+++ e-commerce-app/src/components/Product/Product.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import './Product.css';
+import React from "react";
+import "./Product.css";
 
 const Product = (props) => {
-    const {name,quantity,seller,price,img} = props.product;
-    return (
-        <div className='product'>
-            <img src={img} alt="" srcset="" />
-            <h6>{name}</h6>
-        </div>
-    );
+  const { name, rating, seller, price, img } = props.product;
+  return (
+    <div className="product">
+      <img src={img} alt="" srcset="" />
+      <div className="product-info">
+        <h6 className="product-name">{name}</h6>
+        <p>Price: ${price}</p>
+        <p>Manufacturer: {seller}</p>
+        <p>Rate: {rating} stars</p>
+      </div>
+      <button className="btn-cart">Add to Cart</button>
+    </div>
+  );
 };
 
 export default Product;
