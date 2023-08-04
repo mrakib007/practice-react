5806c8afa4470ea3ad0c47ae06b3732569a45f1d
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-08-04 23:14:29 +0600

    Added some ways to fix quantity issue.

--- e-commerce-app/src/components/Cart/Cart.js
+++ e-commerce-app/src/components/Cart/Cart.js
@@ -6,10 +6,16 @@ const Cart = ({ cart }) => {
   // const {cart} = props; option 2
 
   let totalPrice = 0;
-  let totalShipping = 0
+  let totalShipping = 0;
+  let quantity = 0;
+
   for(const product of cart){
-    totalPrice = totalPrice + product.price;
+    // //If product quantity is zero then set the quantity equal to one.
+    // product.quantity = product.quantity || 1; //easy solution
+
+    totalPrice = totalPrice + product.price * product.quantity;
     totalShipping = totalShipping + product.shipping;
+    quantity = quantity + product.quantity;
   }
 
   const tax = totalPrice*7/100;
@@ -17,7 +23,7 @@ const Cart = ({ cart }) => {
   return (
     <div className="cart">
       <h4>Order Summary</h4>
-      <p>Selected Items: {cart.length}</p>
+      <p>Selected Items: {quantity}</p>
       <p>Total Price: ${totalPrice}</p>
       <p>Shipping: ${totalShipping}</p>
       <p>Tax: ${tax.toFixed(2)}</p>
