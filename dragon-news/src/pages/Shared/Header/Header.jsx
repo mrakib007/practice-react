ccf6825796a4d4e53ef8c37f70cf4a7436f66183
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-08-24 12:12:49 +0600

    Added react marquee

--- dragon-news/src/pages/Shared/Header/Header.jsx
+++ dragon-news/src/pages/Shared/Header/Header.jsx
@@ -1,17 +1,28 @@
-import React from 'react';
-import logo from '../../../assets/logo.png';
-import moment from 'moment';
+import React from "react";
+import logo from "../../../assets/logo.png";
+import moment from "moment";
+import { Button, Container } from "react-bootstrap";
+import Marquee from "react-fast-marquee";
 
 const Header = () => {
-    return (
-        <div>
-            <div className="text-center">
-            <img src={logo} alt="" srcSet="" />
-            <p className='text-secondary'><small>Journalism Without Fear of Favor</small></p>
-            <p>{moment().format("dddd, MMMMM D, YYYY")}</p>
-           </div>
-        </div>
-    );
+  return (
+    <Container>
+      <div className="text-center">
+        <img src={logo} alt="" srcSet="" />
+        <p className="text-secondary">
+          <small>Journalism Without Fear of Favor</small>
+        </p>
+        <p>{moment().format("dddd, MMMMM D, YYYY")}</p>
+      </div>
+      <div className="d-flex">
+        <Button variant="danger">Danger</Button>
+        <Marquee className="text-danger" speed={100}>
+          I can be a React component, multiple React components, or just some
+          text.
+        </Marquee>
+      </div>
+    </Container>
+  );
 };
 
 export default Header;
