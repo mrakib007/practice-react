b16dbafb17a9c45a555b91fd41bb0a3325e4c720
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-08-25 11:02:59 +0600

    Added server for dragon news and fixed some styles

--- dragon-news/src/pages/Shared/Header/Header.jsx
+++ dragon-news/src/pages/Shared/Header/Header.jsx
@@ -6,7 +6,7 @@ import Marquee from "react-fast-marquee";
 
 const Header = () => {
   return (
-    <Container>
+    <Container className="mt-4">
       <div className="text-center">
         <img src={logo} alt="" srcSet="" />
         <p className="text-secondary">
--- dragon-news/src/pages/Shared/RightNav/RightNav.jsx
+++ dragon-news/src/pages/Shared/RightNav/RightNav.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React from "react";
+import { Button, ListGroup } from "react-bootstrap";
+import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
+import QZone from "../QZone/QZone";
+import bg from '../../../assets/bg.png';
 
 const RightNav = () => {
-    return (
-        <div>
-            <h2>Right Nav</h2>
-        </div>
-    );
+  return (
+    <div>
+      <h4 className="mt-4">Login with</h4>
+      <Button className="mb-2" variant="outline-primary">
+        <FaGoogle /> Login With Google
+      </Button>
+      <Button variant="outline-secondary">
+        <FaGithub /> Login with GitHub
+      </Button>
+      <div>
+        <h4 className="mt-4">Find Us On</h4>
+        <ListGroup>
+          <ListGroup.Item><FaFacebook/>facebook</ListGroup.Item>
+          <ListGroup.Item><FaTwitter/>twitter</ListGroup.Item>
+          <ListGroup.Item><FaInstagram/>instagram</ListGroup.Item>
+        </ListGroup>
+      </div>
+
+      <QZone/>
+
+      <div>
+        <img src={bg} alt="" srcSet="" />
+      </div>
+    </div>
+  );
 };
 
 export default RightNav;
