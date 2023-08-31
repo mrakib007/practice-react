9ac4154125088cd95fcea8726c089cfbd3fa619c
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-08-31 15:40:01 +0600

    Added private route but with some issues

--- dragon-news/src/routes/PrivateRoute.jsx
+++ dragon-news/src/routes/PrivateRoute.jsx
@@ -1,12 +1,18 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../providers/AuthProvider';
+import {Navigate, useLocation} from 'react-router-dom';
 
-const PrivateRoute = () => {
+const PrivateRoute = ({children}) => {
     const {user} = useContext(AuthContext);
+    const location = useLocation();
+    console.log(location);
+
+    if(user){
+        return children;
+    }
+
     return (
-        <div>
-            
-        </div>
+        <Navigate state={{from: location}} to={"/login"} replace></Navigate>
     );
 };
 
